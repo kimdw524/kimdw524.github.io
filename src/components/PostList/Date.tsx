bd6796c2870adc762e8d379cc1c383e7f2bdca6f
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-04-23 10:31:16 +0900

    design: Date 포맷 변경 및 아이콘 추가

--- src/components/PostList/Date.tsx
+++ src/components/PostList/Date.tsx
@@ -1,28 +1,41 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
-import React from 'react';
+import { AiOutlineClockCircle } from '@react-icons/all-files/ai/AiOutlineClockCircle';
 
 interface PostListDateProps {
   children: string;
 }
 
 const dateCss = css`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
   padding: 0.5rem 0;
 
   color: var(--postlist-date-color);
+  font-weight: 500;
   font-size: 0.875rem;
   text-align: right;
 `;
 
 const Title = ({ children }: PostListDateProps) => {
   const options: Intl.DateTimeFormatOptions = {
-    weekday: 'long',
     year: 'numeric',
-    month: 'long',
+    month: 'short',
     day: 'numeric',
   };
 
-  return <div css={dateCss}>{new Date(children).toLocaleDateString(undefined, options)}</div>;
+  return (
+    <div css={dateCss}>
+      <AiOutlineClockCircle
+        css={css`
+          margin-right: 0.25rem;
+        `}
+      />
+      {new Date(children).toLocaleDateString('en-US', options)}
+    </div>
+  );
 };
 
 export default Title;
