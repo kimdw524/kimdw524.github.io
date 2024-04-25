17b33fd66d37d1cd59b3d0e5e03d585cf0174e32
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-04-25 15:37:55 +0900

    feat: Post tag 목록 추가

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
-import { graphql, type HeadFC, type PageProps } from 'gatsby';
+import { graphql, Link, type HeadFC, type PageProps } from 'gatsby';
 import Layout from '../components/Layout';
 import useSiteMetadata from '../hooks/useSiteMetadata';
 import { ImageDataLike } from 'gatsby-plugin-image';
@@ -20,12 +20,14 @@ interface PostProps extends PageProps {
 }
 
 const containerCss = css`
+  max-width: var(--body-maxWidth);
+  margin: 0 auto;
   padding: 1rem 2rem;
 `;
 
 const titleCss = css`
   font-weight: 500;
-  font-size: 1.5rem;
+  font-size: 1.75rem;
   line-height: 150%;
 `;
 
@@ -33,11 +35,26 @@ const bodyCss = css`
   line-height: 175%;
 `;
 
+const tagContainerCss = css`
+  margin: 0.5rem 0 1.25rem;
+`;
+
+const tagCss = css`
+  margin-right: 0.5rem;
+`;
+
 const Post = (props: PostProps) => {
   return (
     <Layout location={props.location}>
       <div css={containerCss}>
         <div css={titleCss}>{props.data.mdx.frontmatter.title}</div>
+        <div css={tagContainerCss}>
+          {props.data.mdx.frontmatter.tags.map((tag) => (
+            <Link css={tagCss} key={tag} to="#">
+              {tag}
+            </Link>
+          ))}
+        </div>
         <div css={bodyCss}>{props.data.mdx.body}</div>
       </div>
     </Layout>
