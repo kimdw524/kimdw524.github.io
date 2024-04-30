9c5dff369d274f966a60ecdd1371975123824717
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-04-30 17:19:31 +0900

    refactor: PostList index 합치기

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,9 +1,49 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import { ReactNode, createContext, useState } from 'react';
 import Excerpt from './Excerpt';
-import PostList from './PostList';
 import Title from './Title';
 import Date from './Date';
 import Tag from './Tag';
 import Banner from './Banner';
 import Container from './Container';
+
+interface PostListProps {
+  children: ReactNode;
+}
+
+const postListCss = css`
+  display: inline-block;
+
+  width: 33.3%;
+  padding: 1rem;
+  box-sizing: border-box;
+
+  cursor: pointer;
+
+  @media (max-width: 720px) {
+    width: 50%;
+  }
+
+  @media (max-width: 576px) {
+    width: 100%;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export const PostListContext = createContext<boolean>(false);
+
+const PostList = ({ children }: PostListProps) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div css={postListCss} onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}>
+      <PostListContext.Provider value={hover}>{children}</PostListContext.Provider>
+    </div>
+  );
+};
 
 export default Object.assign(PostList, { Title, Excerpt, Date, Tag, Banner, Container });
