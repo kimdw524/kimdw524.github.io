063aa50e22b9c98a79a61089f4ed6a6e603e9550
kimdw524 <kimdw524@naver.com>
2024-05-02 01:07:46 +0900

    feat: Section Head animation

--- src/components/Section/Head.tsx
+++ src/components/Section/Head.tsx
@@ -1,17 +1,34 @@
 /** @jsx jsx */
-import { css, jsx } from '@emotion/react';
+import { css, jsx, keyframes } from '@emotion/react';
 import { ReactNode } from 'react';
 
 interface SectionHeadProps {
   children: ReactNode;
 }
 
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
 const headCss = css`
+  display: inline-block;
+
   margin: 0.5rem 0;
 
   font-weight: 700;
   letter-spacing: 0.125rem;
   text-transform: uppercase;
+
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
+  animation-duration: 500ms;
+  animation-timing-function: ease;
 `;
 
 const Head = ({ children }: SectionHeadProps) => {
