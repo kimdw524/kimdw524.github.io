e1c1eff8ac33f9673744e165b2b7c476a1890e0b
kimdw524 <kimdw524@naver.com>
2024-06-23 02:59:31 +0900

    feat: gatsby markdown inline image

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -30,6 +30,12 @@ const config: GatsbyConfig = {
               rel: `noopener noreferrer`,
             },
           },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
         ],
       },
     },
@@ -37,6 +43,14 @@ const config: GatsbyConfig = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
       },
     },
     {
