c55355ea09b96473813e5de072fc4c53653a48f5
kimdw524 <kimdw524@naver.com>
2024-12-07 02:02:07 +0900

    feat: Chip variant 추가 (#7)

--- src/components/common/Chip/index.tsx
+++ src/components/common/Chip/index.tsx
@@ -5,10 +5,11 @@ import * as s from './Chip.css';
 interface ChipProps {
   children: ReactNode;
   size?: 'sm' | 'md';
+  variant?: 'contained' | 'outlined';
 }
 
-const Chip = ({ children, size = 'sm' }: ChipProps) => {
-  return <span className={s.chip({ size })}>{children}</span>;
+const Chip = ({ children, size = 'sm', variant = 'contained' }: ChipProps) => {
+  return <span className={s.chip({ size, variant })}>{children}</span>;
 };
 
 export default Chip;
