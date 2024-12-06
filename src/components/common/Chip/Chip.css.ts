478797a0a1446aaca1fd701ef7693031801e653d
kimdw524 <kimdw524@naver.com>
2024-12-07 01:10:53 +0900

    feat: Chip size 추가 (#7)

--- src/components/common/Chip/Chip.css.ts
+++ src/components/common/Chip/Chip.css.ts
@@ -1,18 +1,35 @@
-import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 import colorPalette from '@/styles/colorPalette';
 
-export const chip = style({
-  padding: '0.375rem 0.5rem',
-  borderRadius: '0.75rem',
-  backgroundColor: colorPalette.blueGrey[50],
+export const chip = recipe({
+  base: {
+    backgroundColor: colorPalette.blueGrey[50],
 
-  fontSize: '0.875rem',
-  wordBreak: 'keep-all',
+    wordBreak: 'keep-all',
 
-  transition: 'background-color 0.2s ease',
+    transition: 'background-color 0.2s ease',
 
-  ':hover': {
-    backgroundColor: colorPalette.blueGrey[100],
+    ':hover': {
+      backgroundColor: colorPalette.blueGrey[100],
+    },
+  },
+
+  variants: {
+    size: {
+      sm: {
+        padding: '0.375rem 0.5rem',
+        borderRadius: '0.75rem',
+
+        fontSize: '0.875rem',
+      },
+
+      md: {
+        padding: '0.5rem 0.75rem',
+        borderRadius: '0.875rem',
+
+        fontSize: '1rem',
+      },
+    },
   },
 });
