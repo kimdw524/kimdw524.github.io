afee37ce57384c2c714f2a0f4fb474af2b851427
kimdw524 <kimdw524@naver.com>
2024-12-08 01:06:37 +0900

    fix: 이미 스크롤 되어있는 경우 false 반환하던 문제 해결 (#5)
    초기값 정의할 때는 window.scrollY가 항상 0을 반환해서 window.load에 바인딩함.

--- src/hooks/useIsScrolled/index.ts
+++ src/hooks/useIsScrolled/index.ts
@@ -8,10 +8,16 @@ const useIsScrolled = () => {
       setIsScrolled(window.scrollY !== 0);
     };
 
+    const handleLoad = () => {
+      setIsScrolled(window.scrollY !== 0);
+    };
+
     window.addEventListener('scroll', handleScroll);
+    window.addEventListener('load', handleLoad);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('load', handleLoad);
     };
   }, []);
 
