d27c179ff6627db6a4d5ac24ff23df80acc3824a
kimdw524 <kimdw524@users.noreply.github.com>
2025-05-08 15:04:06 +0000

    deploy: 637c844d38ea45dc12f5896965e67bb85a6d3f2d

---
+++ portfolio/_next/static/chunks/app/layout-a29729e569ad0081.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2453:()=>{},3546:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2453,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[853,878,455,358],()=>s(3546)),_N_E=e.O()}]);
